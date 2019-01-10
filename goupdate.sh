4f52f5cd732d0476cca0ba0ac65f2149565aa250
Amakuchisan <s18t318@stu.kagawa-u.ac.jp>
2019-01-10 20:11:53 +0900

    Fix the double quotes to single quotes

--- goupdate.sh
+++ goupdate.sh
@@ -4,5 +4,5 @@ goenv install 1.11.4
 goenv global 1.11.4
 goenv rehash
 
-echo "export GOPATH=$HOME/go" >> ~/.bashrc
-echo "PATH=$PATH:$GOPATH/bin" >> ~/.bashrc
+echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+echo 'PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
