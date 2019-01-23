d664c5c1b2fbb86fdfd3a98268c940060fdc97a6
pikapika2 <yossii0108@gmail.com>
2019-01-23 21:51:34 +0900

    gto version

---
+++ auth.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+
+    "github.com/markbates/goth/gothic"
+    "github.com/stretchr/objx"
+)
+
+// MustAuth forces user to be authenticated
+func MustAuth(handler http.Handler) http.Handler {
+    return &authHandler{next: handler}
+}
+
+type authHandler struct {
+    next http.Handler
+}
+
+// ③
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    if cookie, err := r.Cookie("auth"); err == http.ErrNoCookie || cookie.Value == "" {
+        // 未承認時はログイン画面へリダイレクト
+        w.Header().Set("Location", "/login")
+        w.WriteHeader(http.StatusTemporaryRedirect)
+    } else {
+        // 承認成功。ラップされたハンドラを呼び出す
+        h.next.ServeHTTP(w, r)
+    }
+}
+
+func callbackHandler(w http.ResponseWriter, r *http.Request) {
+    // ④ 外部サービスからの認証結果を判定
+    user, err := gothic.CompleteUserAuth(w, r)
+    if err != nil {
+        fmt.Fprintln(w, err)
+        return
+    }
+
+    // ⑤ 外部サービスから取得した情報をアプリ用データとしてCookieにしこむ
+    authCookieValue := objx.New(map[string]interface{}{
+        "name":       user.Name,
+        "avatar_url": user.AvatarURL,
+    }).MustBase64()
+    http.SetCookie(w, &http.Cookie{
+        Name:  "auth",
+        Value: authCookieValue,
+        Path:  "/",
+    })
+
+    // メイン画面へリダイレクト
+    w.Header()["Location"] = []string{"/"}
+    w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func logoutHandler(w http.ResponseWriter, r *http.Request) {
+    http.SetCookie(w, &http.Cookie{
+        Name:   "auth",
+        Value:  "",
+        Path:   "/",
+        MaxAge: -1,
+    })
+
+    w.Header()["Location"] = []string{"/"}
+    w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
--- main.go
+++ main.go
@@ -2,23 +2,71 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"html/template"
 	"net/http"
 	"os"
+	"path/filepath"
+	"sync"
+	//"text/template"
+
+	"github.com/gorilla/pat"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/github"
+	"github.com/stretchr/objx"
 )
+
+func init(){
+	goth.UseProviders(
+		github.New(os.Getenv("GOSIMPLEWEBAPP_GITHUB_ID"), os.Getenv("GOSIMPLEWEBAPP_GITHUB_SECRET"), "http://localhost:3000/auth/github/callback"),
+	)
+}
+
+type templateHandler struct {
+	filename string
+	once     sync.Once
+	templ   *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	t.once.Do(func() {
+		t.templ = template.Must(
+			template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+
+	data := make(map[string]interface{})
+
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["Userdata"] = objx.MustFromBase64(authCookie.Value)
+	}
+
+	t.templ.Execute(w, data)
+}
 
 func main() {
 
-	server := http.Server{
+	/*server := http.Server{
 		Addr:    "127.0.0.1:8080",
 		Handler: nil,
-	}
+	}*/
 
-	http.HandleFunc("/", indexHandler)
+	//http.HandleFunc("/", indexHandler)
 
-	server.ListenAndServe()
+	p := pat.New()
+	p.Get("/auth/{provider}/callback", callbackHandler)
+	p.Get("/auth/{provider}", gothic.BeginAuthHandler)
+	p.Get("/logout", logoutHandler)
+	p.Add("GET", "/login", (&templateHandler{filename: "login.html"}))
+	p.Add("GET", "/", MustAuth(&templateHandler{filename: "index.html"}))
+
+	//server.ListenAndServe()
+
+	log.Fatal(http.ListenAndServe(":3000", p))
 
 }
+
+
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 
