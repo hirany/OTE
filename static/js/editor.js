ae47e8b09dac6a68007e0e87d8fc6e0f6f8aca41
Amakuchisan <s18t318@stu.kagawa-u.ac.jp>
2019-01-23 22:22:46 +0900

    editorのchangeを検知

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,9 +1,15 @@
-window.addEventListener("load", () => {
+function roadEditor() {
+	const save = document.getElementById('save')
 	let editor = ace.edit("editor");
 	editor.setTheme("ace/theme/monokai");
 	editor.setFontSize(14);
 	editor.getSession().setMode("ace/mode/html");
 	editor.getSession().setUseWrapMode(true); //折り返し
 	editor.getSession().setTabSize(2); //タブ幅
-})
+	save.addEventListener("click", () => {
+		saveFile(editor)
+	})
+	editor.addEventListener("change", () => {console.log(val)})
+}
 
+window.addEventListener("load", () => roadEditor())
