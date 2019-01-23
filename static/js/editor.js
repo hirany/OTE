29b6d18e09dd28ca1a3c0a05260dda6448a42c71
Amakuchisan <s18t318@stu.kagawa-u.ac.jp>
2019-01-23 22:44:32 +0900

    fix typo

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,15 +1,20 @@
-function roadEditor() {
-	const save = document.getElementById('save')
+function loadEditor() {
+//	const save = document.getElementById('save')
 	let editor = ace.edit("editor");
 	editor.setTheme("ace/theme/monokai");
 	editor.setFontSize(14);
 	editor.getSession().setMode("ace/mode/html");
 	editor.getSession().setUseWrapMode(true); //折り返し
 	editor.getSession().setTabSize(2); //タブ幅
-	save.addEventListener("click", () => {
-		saveFile(editor)
-	})
-	editor.addEventListener("change", () => {console.log(val)})
+//	save.addEventListener("click", () => {
+//		saveFile(editor)
+//	})
+	editor.addEventListener("change", () => {console.log(editor.getValue())})
 }
 
-window.addEventListener("load", () => roadEditor())
+window.addEventListener("load", () => loadEditor())
+
+
+function saveFile(editor) {
+	console.log(editor)
+}
